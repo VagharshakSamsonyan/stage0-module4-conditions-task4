60c1017c4c8d18977b2cf88c3485cdc1f59d35b5
Vagharshak Samsonyan <vagosamsonyan@yahoo.com>
2024-03-31 16:21:34 +0400

    20 of 20 tests passed

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int result = (first>second) ?10:-10;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        TernaryAssignment ternaryAssignment = new TernaryAssignment();
+        ternaryAssignment.assignAndPrintBasedOnWhichBigger(15,5);
+        ternaryAssignment.assignAndPrintBasedOnWhichBigger(5,15);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        System.out.println(((first > second) ? "first" : "second"));
+    }
+
+    public static void main(String[] args) {
+        TernaryPrinter printer = new TernaryPrinter();
+        printer.printWhichIsBigger(5,10);
+        printer.printWhichIsBigger(8,3);
+        printer.printWhichIsBigger(5,5);
     }
 }
